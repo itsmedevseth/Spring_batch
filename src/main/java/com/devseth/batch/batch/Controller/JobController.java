91a10dbb949caf9b7b65143235ad21e0f78efd1e
Piseth Nem <nem.devseth@gmail.com>
2022-11-29 20:32:24 +0700

    JobContro

---
+++ src/main/java/com/devseth/batch/batch/Controller/JobController.java
@@ -0,0 +1,9 @@
+package com.devseth.batch.batch.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/job")
+public class JobController {
+}
